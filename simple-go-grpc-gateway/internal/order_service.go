760bede5e16eb5f17abe68df46e7edd10cee7f78
Ha Manh Do <v.hadm7@vinai.io>
2024-06-11 16:08:47 +0700

    Add remaining REST endpoints for orders

--- simple-go-grpc-gateway/internal/order_service.go
+++ simple-go-grpc-gateway/internal/order_service.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"context"
+	"fmt"
 	"github.com/hwebz/simple-go-grpc-gateway/protogen/golang/orders"
 	"log"
 )
@@ -26,4 +27,34 @@ func (o *OrderService) AddOrder(_ context.Context, req *orders.PayloadWithSingle
 	err := o.db.AddOrder(req.GetOrder())
 
 	return &orders.Empty{}, err
+}
+
+// UpdateOrder implements the UpdateOrder method of the grpc OrdersServer interface to update an order
+func (o *OrderService) UpdateOrder(_ context.Context, req *orders.PayloadWithSingleOrder) (*orders.Empty, error) {
+	log.Printf("Received an update order request")
+
+	o.db.UpdateOrder(req.GetOrder())
+
+	return &orders.Empty{}, nil
+}
+
+// GetOrder implements the GetOrder method of the grpc OrdersServer interface to fetch an order for a given orderID
+func (o *OrderService) GetOrder(_ context.Context, req *orders.PayloadWithOrderID) (*orders.PayloadWithSingleOrder, error) {
+	log.Printf("Received get order request with orderID: %d", req.GetOrderId())
+
+	order := o.db.GetOrderByID(req.GetOrderId())
+	if order == nil {
+		return nil, fmt.Errorf("Order not found for the orderID: %d", req.GetOrderId())
+	}
+
+	return &orders.PayloadWithSingleOrder{Order: order}, nil
+}
+
+// RemoveOrder implements the RemoveOrder method of the grpc OrdersServer interface to remove an order
+func (o *OrderService) RemoveOrder(_ context.Context, req *orders.PayloadWithOrderID) (*orders.Empty, error) {
+	log.Printf("Received remove order request")
+
+	o.db.RemoveOrder(req.GetOrderId())
+
+	return &orders.Empty{}, nil
 }

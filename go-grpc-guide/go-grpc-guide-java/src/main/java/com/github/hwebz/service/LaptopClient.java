5d4930cc7fe7fded4ad84316975b1d250d7d13e8
Ha Manh Do <v.hadm7@vinai.io>
2024-06-20 10:35:46 +0700

    Update laptop client from Java project

--- go-grpc-guide/go-grpc-guide-java/src/main/java/com/github/hwebz/service/LaptopClient.java
+++ go-grpc-guide/go-grpc-guide-java/src/main/java/com/github/hwebz/service/LaptopClient.java
@@ -2,28 +2,38 @@ package com.github.hwebz.service;
 
 import com.github.hwebz.grpc.*;
 import com.github.hwebz.sample.Generator;
+import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.sql.Time;
 import java.util.Iterator;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class LaptopClient {
     private static final Logger logger = Logger.getLogger(LaptopClient.class.getName());
+    private static final Log log = LogFactory.getLog(LaptopClient.class);
 
     private final ManagedChannel channel;
     private final LaptopServiceGrpc.LaptopServiceBlockingStub blockingStub;
+    private final LaptopServiceGrpc.LaptopServiceStub asyncStub;
 
     public LaptopClient(String host, int port) {
         channel = ManagedChannelBuilder.forAddress(host, port)
                 .usePlaintext()
                 .build();
         blockingStub = LaptopServiceGrpc.newBlockingStub(channel);
+        asyncStub = LaptopServiceGrpc.newStub(channel);
     }
 
     public void shutdown() throws InterruptedException {
@@ -71,29 +81,100 @@ public class LaptopClient {
         logger.info("Search completed");
     }
 
+    public void uploadImage(String laptopID, String imagePath) throws InterruptedException {
+        CountDownLatch finishLatch = new CountDownLatch(1);
+        StreamObserver<UploadImageRequest> requestStreamObserver = asyncStub.withDeadlineAfter(5, TimeUnit.SECONDS).uploadImage(new StreamObserver<UploadImageResponse>() {
+            @Override
+            public void onNext(UploadImageResponse uploadImageResponse) {
+                logger.info("Receive response: " + uploadImageResponse);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                logger.log(Level.SEVERE, "Upload failed: " + throwable);
+                finishLatch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                logger.info("Image uploaded");
+                finishLatch.countDown();
+            }
+        });
+
+        FileInputStream fileInputStream;
+        try {
+            fileInputStream = new FileInputStream(imagePath);
+        } catch (FileNotFoundException e) {
+            logger.log(Level.SEVERE, "Cannot read image file: " + e.getMessage());
+            return;
+        }
+
+        String imageType = imagePath.substring(imagePath.lastIndexOf("."));
+        ImageInfo info = ImageInfo.newBuilder().setLaptopId(laptopID).setImageType(imageType).build();
+        UploadImageRequest request = UploadImageRequest.newBuilder().setInfo(info).build();
+
+        try {
+            requestStreamObserver.onNext(request);
+            logger.info("Image info sent: " + info);
+
+            byte[] buffer = new byte[1024];
+            while (true) {
+                int n = fileInputStream.read(buffer);
+                if (n <= 0) {
+                    break;
+                }
+
+                if (finishLatch.getCount() == 0) {
+                    return;
+                }
+
+                request = UploadImageRequest.newBuilder()
+                        .setChunkData(ByteString.copyFrom(buffer, 0, n))
+                        .build();
+                requestStreamObserver.onNext(request);
+                logger.info("Image chunk sent with size: " + n);
+            }
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Unexpected error: " + e.getMessage());
+            requestStreamObserver.onError(e);
+        }
+
+        requestStreamObserver.onCompleted();
+
+        if (!finishLatch.await(1, TimeUnit.MINUTES)) {
+            logger.warning("Request cannot finish within 1 minute");
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException {
         LaptopClient client = new LaptopClient("0.0.0.0", 8089);
 
         Generator generator = new Generator();
 //        Laptop laptop = generator.NewLaptop();
         // Laptop laptop = generator.NewLaptop().toBuilder().setId("5934909a-ac53-4657-9141-dfb895e290bc").build(); // Already exists
-        // Laptop laptop = generator.NewLaptop().toBuilder().setId("invalid-uuid").build(); // Invalid ID
+        // Laptop laptop = generator.NewLaptop().toBuilder().setId("invalid-uuid").build(); // Invalid ID/home/vinai/learning/learn-go/go-grpc-guide/tmp/laptop.png
 
         try {
-            for (int i = 0; i < 10; i++) {
-                Laptop laptop = generator.NewLaptop();
-                client.createLaptop(laptop);
-            }
+//            for (int i = 0; i < 10; i++) {
+//                Laptop laptop = generator.NewLaptop();
+//                client.createLaptop(laptop);
+//            }
+//
+//            Memory minRam = Memory.newBuilder().setValue(4).setUnit(Memory.Unit.GIGABYTE).build();
+//            Filter filter = Filter.newBuilder()
+//                    .setMaxPriceUsd(3000)
+//                    .setMinCpuCores(4)
+//                    .setMinCpuGhz(2.0)
+//                    .setMinRam(minRam)
+//                    .build();
+//
+//            client.searchLaptop(filter);
 
-            Memory minRam = Memory.newBuilder().setValue(4).setUnit(Memory.Unit.GIGABYTE).build();
-            Filter filter = Filter.newBuilder()
-                    .setMaxPriceUsd(3000)
-                    .setMinCpuCores(4)
-                    .setMinCpuGhz(2.0)
-                    .setMinRam(minRam)
-                    .build();
-
-            client.searchLaptop(filter);
+            // Test upload laptop image
+            Laptop laptop = generator.NewLaptop();
+            client.createLaptop(laptop);
+            client.uploadImage(laptop.getId(), "tmp/laptop.png");
         } finally {
             client.shutdown();
         }
